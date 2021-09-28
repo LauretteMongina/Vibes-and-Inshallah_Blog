57efff770030773e56158f30eb10931d18c5144d
laurettemongina <monginalaurette2gmail.com>
2021-09-28 17:23:47 +0300

    fixing development

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ class Config:
 
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    CSRF_ENABLED = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
 

7f3cc3c90e7b8543a9d4c7c69d178af4bb1ea19f
laurettemongina <monginalaurette2gmail.com>
2021-09-28 17:49:41 +0300

    fixing deployment

--- migrations/versions/b1badcc8dc9d_.py
+++ migrations/versions/b1badcc8dc9d_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: d545af6231ca
+Revision ID: b1badcc8dc9d
 Revises: 
-Create Date: 2021-09-28 17:36:17.555767
+Create Date: 2021-09-28 17:48:14.530250
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'd545af6231ca'
+revision = 'b1badcc8dc9d'
 down_revision = None
 branch_labels = None
 depends_on = None

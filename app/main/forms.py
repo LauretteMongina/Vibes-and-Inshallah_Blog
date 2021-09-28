4c7a4e9336cf8294d91e9cf07fbc1404e83203fd
laurettemongina <monginalaurette2gmail.com>
2021-09-28 14:07:42 +0300

    Creates a subscribe,comment,blogpost and update profile forms.

---
+++ app/main/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField, SelectField
+from wtforms.validators import Required
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class Blog_postForm(FlaskForm):
+    post_title = StringField('Title')
+    description = TextAreaField('Description', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Write a comment', validators=[Required()])
+    submit = SubmitField('Comment')
+    
+    
+class SubscriberForm(FlaskForm):
+    email = TextAreaField('Email', validators=[Required()])
+    submit = SubmitField('Submit')  

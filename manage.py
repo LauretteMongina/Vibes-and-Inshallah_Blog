d9f045023e52913edbd9759fd3e231746c70633e
laurettemongina <monginalaurette2gmail.com>
2021-09-28 14:06:07 +0300

    adds a manage.py file to run the application.

---
+++ manage.py
@@ -0,0 +1,26 @@
+from app import create_app,db
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+from app.models import User,BlogPost,Comment,Subscriber,Category
+
+app = create_app('development')
+
+manager = Manager(app)
+migrate = Migrate(app,db)
+
+manager.add_command('db',MigrateCommand)
+manager.add_command('server',Server(use_debugger=True))
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User,Comment = Comment,Blog_post = Blog_Post,Subscriber = Subscriber)
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+if __name__ == "__main__":
+    manager.run()

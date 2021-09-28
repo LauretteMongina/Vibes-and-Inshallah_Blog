25c250fb3d15d0e12db53e65191c6273cb16c644
laurettemongina <monginalaurette2gmail.com>
2021-09-28 19:00:06 +0300

    fixing deployment

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -11,16 +11,17 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Enter Your Username', validators=[Required(), Length(min=4, max=20)])
-    email = StringField('Email Address', validators=[Required(),Email()])
-    password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
-    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    email = StringField('Your Email Address', validators=[Required(), Email()])
+    username = StringField('Enter your username', validators=[Required()])
+    password = PasswordField('Password',
+                             validators=[Required(), EqualTo('password_confirm', message='Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords', validators=[Required()])
     submit = SubmitField('Sign Up')
 
-    def validate_email(self,data_field):
-        if User.query.filter_by(email = data_field.data).first():
-            raise ValidationError(message="The Email has already been taken!")
-    
+    def validate_email(self, data_field):
+        if User.query.filter_by(email=data_field.data).first():
+            raise ValidationError('There is an account with that email')
+
     def validate_username(self, data_field):
-        if User.query.filter_by(username = data_field.data).first():
-            raise ValidationError(message="The Username has already been taken")
+        if User.query.filter_by(username=data_field.data).first():
+            raise ValidationError('That username is taken')

8f2bbd5245bdb2f6cd7310bc70a284e5d7bb103e
laurettemongina <monginalaurette2gmail.com>
2021-09-29 08:21:16 +0300

    Adds urlfor login function.

--- app/auth/views.py
+++ app/auth/views.py
@@ -13,13 +13,12 @@ def login():
         user = User.query.filter_by(email = login_form.email.data).first()
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user,login_form.remember.data)
-            next = request.args.get('next')
-            if next == None or not next[0]=='/':
-                next = (url_for('main.index'))
-            return redirect(next)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
 
     title = "Login to your account"
-    return render_template('auth/login.html',login_form = login_form, title = title)
+    return render_template('auth/login.html', login_form = login_form, title = title)
 
 @auth.route('/logout')
 @login_required
